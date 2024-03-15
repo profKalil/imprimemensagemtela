c35ddfb399d5cefdcc0853136261a4b8616c8466
profKalil <84177112+profKalil@users.noreply.github.com>
2024-03-15 11:00:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Programa para imprimir uma mensagem na tela
+
+#Entradas com as variaveis
+# mensagem = "Olá mundo"
+numero1 = 10
+numero2 = 2
+
+#Processos
+soma = numero1 + numero2
+
+#Método de saída
+# print(mensagem)
+print("O resultado da soma é ", soma)
