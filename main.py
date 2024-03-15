144cc48fc33c3f5052936f25c1014c1243550476
profKalil <84177112+profKalil@users.noreply.github.com>
2024-03-15 11:16:38 +0000

    Programa usa operadores matemáticos

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 
 #Entradas com as variaveis
 # mensagem = "Olá mundo"
-numero1 = 10
+numero1 = 49
 numero2 = 2
 
 #Processos
-soma = numero1 + numero2
+resultado = numero1 ** (1/numero2)
 
 #Método de saída
 # print(mensagem)
-print("O resultado da soma é ", soma)
+# print("O resultado da soma de ", numero1, " e ", numero2, " é ",  soma)
+# print (f"O resultado da soma de {numero1} e {numero2} é {soma}")
+print ("O resultado é {}".format(resultado))
